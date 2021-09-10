5067576a6ae3c1153f97ee61b0c3879787c227fd
João Vitor Reis Ferreira <81247955+joaovrferreira@users.noreply.github.com>
2021-09-10 10:14:32 -0300

    Delete condição_simples.js

--- condição_simples.js
+++
@@ -1,6 +0,0 @@
-var vel = 60.5
-console.log(`A velocidade do seu carro é ${vel}km/h.`)
-if (vel > 60) { // Condição Simples
-    console.log("Você ultrapassou a velocidade permitida. MULTADO.")
-}
-console.log("Dirija sempre com cuidado!")
